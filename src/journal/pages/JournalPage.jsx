8e3d54fb8cfad24d5ab85001499a888ae439febd
lanzarot <luis-389@hotmail.com>
2022-10-10 13:10:38 -0500

    add: views

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,13 @@
 import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Commodo sint adipisicing duis magna consectetur qui ipsum commodo nisi irure sunt reprehenderit laborum reprehenderit. Id aliqua quis qui aliqua tempor eu aliquip cupidatat id enim eu ipsum. Qui aliqua consectetur reprehenderit elit veniam laborum irure irure enim esse cupidatat incididunt consequat ad. Ex sint tempor amet laborum velit elit qui. Eu ex minim nostrud amet minim sunt reprehenderit incididunt in adipisicing et irure.</Typography>
+      {/* <Typography>Commodo sint adipisicing duis magna consectetur qui ipsum commodo nisi irure sunt reprehenderit laborum reprehenderit. Id aliqua quis qui aliqua tempor eu aliquip cupidatat id enim eu ipsum. Qui aliqua consectetur reprehenderit elit veniam laborum irure irure enim esse cupidatat incididunt consequat ad. Ex sint tempor amet laborum velit elit qui. Eu ex minim nostrud amet minim sunt reprehenderit incididunt in adipisicing et irure.</Typography> */}
 
-      {/* Nothing selected */}
+      <NothingSelectedView />
       {/* NoteView */}
 
     </JournalLayout>
