747c212cfc70fed3fc8f3fef5804d5e6664a14eb
lanzarot <luis-389@hotmail.com>
2022-10-14 13:34:57 -0500

    Create new note

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,18 @@
 import { AddOutlined } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
+import { useDispatch } from 'react-redux';
+import { startNewNote } from '../../store/journal';
 import { JournalLayout } from '../layout/JournalLayout';
 import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
+
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () => {
+    dispatch( startNewNote() );
+  }
+
   return (
     <JournalLayout>
       {/* <Typography>Commodo sint adipisicing duis magna consectetur qui ipsum commodo nisi irure sunt reprehenderit laborum reprehenderit. Id aliqua quis qui aliqua tempor eu aliquip cupidatat id enim eu ipsum. Qui aliqua consectetur reprehenderit elit veniam laborum irure irure enim esse cupidatat incididunt consequat ad. Ex sint tempor amet laborum velit elit qui. Eu ex minim nostrud amet minim sunt reprehenderit incididunt in adipisicing et irure.</Typography> */}
@@ -12,6 +21,7 @@ export const JournalPage = () => {
       {/* <NoteView /> */}
 
       <IconButton
+        onClick={ onClickNewNote } 
         size='medium'
         sx={{
           color: 'white',
