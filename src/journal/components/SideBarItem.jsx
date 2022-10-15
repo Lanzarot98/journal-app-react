938ba40fa3c91277d6a1d0eb2da04b5846d5f779
lanzarot <luis-389@hotmail.com>
2022-10-15 10:15:20 -0500

    active note

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -1,9 +1,13 @@
 import { TurnedInNot } from "@mui/icons-material"
 import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
 import { useMemo } from "react";
+import { useDispatch } from "react-redux";
+import { setActiveNote } from "../../store/journal";
 
 
-export const SideBarItem = ({ title = '', body, id }) => {
+export const SideBarItem = ({ title = '', body, id, date, imageUrls = [] }) => {
+
+    const dispatch = useDispatch();
 
     const newTitle = useMemo( () => {
         return title.length > 17
@@ -11,15 +15,25 @@ export const SideBarItem = ({ title = '', body, id }) => {
             : title;
     }, [ title ] )
 
+    const newBody = useMemo( () => {
+        return body.length > 50
+            ? body.substring(0, 50) + '...'
+            : body;
+    }, [ body ] )
+
+    const onClickSetActiveNote = () => {
+        dispatch( setActiveNote({ title, body, id, date, imageUrls }) )
+    }
+
   return (
     <ListItem key={ id } disablePadding>
-        <ListItemButton>
+        <ListItemButton onClick={ onClickSetActiveNote }>
             <ListItemIcon>
                 <TurnedInNot />
             </ListItemIcon>
             <Grid container>
                 <ListItemText primary={ newTitle } />
-                <ListItemText secondary={ body } />
+                <ListItemText secondary={ newBody } />
             </Grid>
         </ListItemButton>
     </ListItem>
