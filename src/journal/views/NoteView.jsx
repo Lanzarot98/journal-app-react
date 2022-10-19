85359c56bc3dc3ccf1bc9c918fe59b7e0ffde519
lanzarot <luis-389@hotmail.com>
2022-10-19 10:54:11 -0500

    Select files from React

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { SaveOutlined } from "@mui/icons-material";
-import { Button, Grid, TextField, Typography } from "@mui/material";
+import { SaveOutlined, UploadOutlined } from "@mui/icons-material";
+import { Button, Grid, IconButton, TextField, Typography } from "@mui/material";
 import Swal from 'sweetalert2';
 import 'sweetalert2/dist/sweetalert2.css';
 
@@ -21,8 +21,9 @@ export const NoteView = () => {
     const dateString = useMemo(() => {
         const newDate = new Date( date );
         return newDate.toUTCString();
-        
     }, [date])
+
+    const fileInputRef = useRef()
 
     useEffect(() => {
       dispatch( setActiveNote( formState ) );
@@ -39,6 +40,13 @@ export const NoteView = () => {
         dispatch( startSaveNote() );
     }
 
+    const onFileInputChange = ({ target }) => {
+        if( target.files === 0 ) return;
+
+        console.log('subiendo archivos');
+        dispatch( startUploadingFiles( target.files ) )
+    }
+
     return (
         <Grid 
             container direction='row' 
@@ -51,6 +59,22 @@ export const NoteView = () => {
                 <Typography fontSize={ 39 } fontWeight='light' >{ dateString }</Typography>
             </Grid>
             <Grid item>
+
+                <input 
+                    type="file"
+                    multiple
+                    ref={ fileInputRef }
+                    onChange={ onFileInputChange }
+                    style={{ display: 'none' }}
+                />
+
+                <IconButton
+                    color="primary"
+                    disabled={ isSaving }
+                >
+                    <UploadOutlined />
+                </IconButton>
+
                 <Button 
                     disabled={ isSaving }
                     onClick={ onSaveNote }
@@ -58,7 +82,7 @@ export const NoteView = () => {
                     sx={{ padding: 2 }}
                 >
                     <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
-                    Guardar
+                    Save
                 </Button>
             </Grid>
 
